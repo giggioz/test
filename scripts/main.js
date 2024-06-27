356a7ad13ed3305649e2b08c5bd234f8436d1062
giggioz <giggioz@gmail.com>
2024-06-27 08:14:40 +0200

    test

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ import { registerSettings } from "./settings.js";
 export const MODULE_ID = "module-id";
 
 Hooks.on("init", () => {
+    console.log('GIGGIO TEST')
     initConfig();
     registerSettings();
 });
